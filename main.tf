a032fdc9fde659f6cbbd3160135e8fbc7c72aa2e
Sri_Devops <164357687+sri1650@users.noreply.github.com>
2026-03-12 18:32:16 +0000

    Initialize AWS infrastructure with VPC and security group
    Define AWS provider, VPC, subnet, and security group.

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_security_group" "web_sg" {
+  name   = "web-sg"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
