6015eddaef32acef410401aec7002638475afdcc
Sri_Devops <164357687+sri1650@users.noreply.github.com>
2026-03-13 00:26:13 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami                         = "ami-0c76bd4bd302b30ec"
-  instance_type               = "t2.micro"
+  instance_type               = "t3.micro"
   subnet_id                   = aws_subnet.main.id
   vpc_security_group_ids      = [aws_security_group.web.id]
   associate_public_ip_address = true
